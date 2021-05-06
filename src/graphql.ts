f0be4335204e61808e0504c5e66a54ecc8b76407
Eric Marcantonio <eric11marcantonio@gmail.com>
2021-05-05 22:55:45 -0400

    init

---
+++ src/graphql.ts
@@ -0,0 +1,17 @@
+import {ApolloServer, gql} from 'apollo-server-lambda'
+
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+`
+
+const resolvers = {
+    Query: {
+        hello: () => "Hello World"
+    }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+export const graphqlHandler = server.createHandler()
